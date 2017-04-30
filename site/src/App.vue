41737f97619de42858c11ab5c1c993b80d589718
Daniel Woodlins <dacjames@gmail.com>
2017-04-30 14:46:43 -0700

    Add Skeleton Site
    Using Vue and Typescript. Still requires babel from the template,
    but planning to remove.

---
+++ site/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app">
+    <img src="./assets/PinkSquareLogo.jpg">
+    <h1>{{ title }}</h1>
+    <h2>{{ subtitle }}</h2>
+    <pre>
+      <code>
+      def fib(x: Int) -> Int {
+        if x <= 1 { 1 }
+        else { self(x - 2) + self(x - 1) }
+      }
+
+      def main(args: List(String)) {
+        fib(10).show
+      }
+      </code>
+    </pre>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'app',
+  data(): Object {
+    return {
+      title: 'Mara',
+      subtitle: 'A fun new language',
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+pre {
+  text-align: left;
+  margin: auto;
+  width: 50%;
+  border: 1px solid black;
+  border-radius: 5px;
+}
+
+</style>
---
+++ site/src/main.ts
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App = require('./App.vue')
+
+// tslint:disable-next-line:no-unused-new
+new Vue({
+  el: '#app',
+  render: (h) => h(App),
+})
